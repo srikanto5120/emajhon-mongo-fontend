90f6548798a3c5ad190d205f7fef8730b0b95ad0
srikanto5120 <shreekantaray@gmail.com>
2021-10-27 15:40:06 +0600

    added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDrByaP3hP5McYKEglJo3cUUx_M365coOo",
+  authDomain: "emajhon-mongo.firebaseapp.com",
+  projectId: "emajhon-mongo",
+  storageBucket: "emajhon-mongo.appspot.com",
+  messagingSenderId: "867803266340",
+  appId: "1:867803266340:web:0f765072695699e18e87b1",
+};
+
+export default firebaseConfig;
+
+/*
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+}; */
